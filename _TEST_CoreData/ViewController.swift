6290f958e0c3d86e30898ff6eedd68b940f42c45
leanne <leanne@Quadzilla.local>
2016-10-16 18:08:57 -0500

    working model fetch request template

--- _TEST_CoreData/ViewController.swift
+++ _TEST_CoreData/ViewController.swift
@@ -7,12 +7,51 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		// set up model parts for request
+		let app = AppDelegate()
+		let container = app.persistentContainer
+		let mainContext = container.viewContext
+		let model = container.managedObjectModel
+		
+		// create fetch request from template (duplicate type declaration just to show myself
+		//	that this is a parameterized generics typed request
+		let cheapProductsFromCountryRequest: NSFetchRequest<Product> =
+			model.fetchRequestFromTemplate(withName: "CheapProductsFromCountry",
+			                               substitutionVariables: ["PRICE": 20.00, "COUNTRY_CD": "CN"])
+		as! NSFetchRequest<Product>
+		
+		// perform fetch based on request
+		guard let cheapProductsFromCountry: [Product] = try? mainContext.fetch(cheapProductsFromCountryRequest) else {
+			print("***** Unable to retrieve Products From Country list *****")
+			return
+		}
+		
+		// print results of fetch
+		for (idx, product) in cheapProductsFromCountry.enumerated() {
+			if let productName = product.name, let mfrName = product.manufacturer?.name, let countryName = product.manufacturer?.country?.name {
+			
+				print("Product \(idx + 1): ", terminator: "")
+				
+				let nameString = "\(productName)\n"
+				let mfrString = "Mfr:\t\t\(mfrName)\n"
+				let countryString = "Country:\t\(countryName)\n"
+				let priceString = "Price:\t%.2f\n"
+				
+				let fullString = nameString + mfrString + countryString + priceString
+				
+				let formattedString = String(format: fullString, product.price)
+				
+				print(formattedString)
+			}
+		}
+		
 	}
 
 
